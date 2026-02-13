3c7dcaa6b42de2718df1ec2a7a965b586c255f9b
2004-maste <rugiravivien@gmail.com>
2026-02-13 12:40:37 +0200

    Delete backend directory

--- backend/app/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Pydantic schemas for API requests and responses.
-"""
-from app.schemas.article import *
-from app.schemas.source import *
-from app.schemas.user import *
-from app.schemas.category import *
